98196dcdcbbda21b875f73130cfa896f632a960b
Alexis <alexis.toledo@live.com>
2021-05-16 19:26:42 +0200

    homepage pointer finished

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './styles/Home.css'
 import HalfScreen from './HalfScreen'
+import Pointer from './Pointer'
 import { HALF_FRONTEND, HALF_DESIGN } from '../constants/Data'
 
 export const Home = () => {
     return (
         <div className='container'>
+            <Pointer />
             <HalfScreen data={HALF_FRONTEND} />
             <HalfScreen data={HALF_DESIGN} />
         </div>
