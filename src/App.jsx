c52e3f248c62ddb0dbf86bc48729b3fbf9afdd14
Alexis Toledo <alexis.toledo@live.com>
2022-06-27 16:36:29 +0200

    adds hidden console message

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import Router from "./components/Router";
 import "./constants/globalStyles.css";
 
 function App() {
+    console.log(
+        "%cHi!, I can see you are curious enough to reach this message...\n\nIf you’re looking for someone like you to fit into your front-end team, feel free to contact me :)",
+        "font-size: 20px; color: white; background: black",
+    );
     return <Router />;
 }
 
